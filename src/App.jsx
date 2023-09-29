7db3716246039907641fa1df193525be7354aeb7
SHIJIN ABRAHAM <shijinabraham2003@gmail.com>
2023-09-29 07:46:37 +0530

    Added Components & Pages in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import Home from './Pages/Home/Home'
+import About from './Pages/About/About'
+import Services from './Pages/Services/Services'
+import Testimonials from './Pages/Testimonials/Testimonials'
+import Footer from './Components/Footer/Footer'
 
 function App() {
 
   return (
-    <div>
-      <Navbar />
+    <div className='App'>
+      <div className="header"><Navbar /></div>
+      <div className="main-body">
+        <Home />
+        <About />
+        <Services />
+        <Testimonials />
+      </div>
+      <div className="footer"><Footer /></div>
     </div>
   )
 }

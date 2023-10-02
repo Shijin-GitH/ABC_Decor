6482e4a7c1c3fafe3b2968256d4606000cd92bb3
SHIJIN ABRAHAM <shijinabraham2003@gmail.com>
2023-10-02 23:03:28 +0530

    Loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import './App.css'
+import {useState} from 'react';
+import { useEffect } from 'react';
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Pages/Home/Home'
 import About from './Pages/About/About'
 import Services from './Pages/Services/Services'
 import Testimonials from './Pages/Testimonials/Testimonials'
 import Footer from './Components/Footer/Footer'
+import Loader from './Components/Loader/Loader'
 
 function App() {
-  
+  const [loader, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 4000)
+  }, [])
+
   return (
     <div className='App'>
+      {loader && <Loader />}
       <div className="header"><Navbar /></div>
       <div className="main-body">
         <Home />
---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Loader.css";
+
+function Loader() {
+  return (
+    <div>
+        <div id="ring-container">
+            <div id="ring">
+                <span></span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Loader
